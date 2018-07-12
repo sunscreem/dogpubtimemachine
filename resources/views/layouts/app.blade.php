52c2e9ff955ce94fad74784c7a6ed411245c464a
Rob Cooper <sunscreem@gmail.com>
2018-07-12 16:17:48 +0100

    added a meta description for the googs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta description="Whats on tap, right, now at all the Brewdog Bars. Includes historical data too.">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
