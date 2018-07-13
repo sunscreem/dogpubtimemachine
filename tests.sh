afb62c2ca4c08d26b12ddc583e2617a7b025d0df
Rob Cooper <sunscreem@gmail.com>
2018-07-13 14:39:49 +0100

    more travis fun

---
+++ tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Trigger deployment
+# Replace the url below with your Forge/Laravel url
+curl -s 'https://forge.laravel.com/servers/150528/sites/563844/deploy/http?token=fzUvpUEoISTrgHR7CeP5KlLtemfzc5WgVcpFXtr8';
+echo 'Deployment triggered!'
