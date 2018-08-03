7a168bd47de6aefdbbd3891a3cda3a43048127f9
Rob Cooper <sunscreem@gmail.com>
2018-08-03 18:17:15 +0100

    nonsense update to see if caching worked in travis

--- app/Bar.php
+++ app/Bar.php
@@ -22,19 +22,10 @@ class Bar extends Model
 
     public static function createWithAttributes(array $attributes): Bar
     {
-        /*
-         * Let's generate a uuid.
-         */
         $attributes['uuid'] = (string) Uuid::uuid4();
 
-        /*
-         * The account will be created inside this event using the generated uuid.
-         */
         event(new BarCreated($attributes));
 
-        /*
-         * The uuid will be used the retrieve the created account.
-         */
         return static::uuid($attributes['uuid']);
     }
 
